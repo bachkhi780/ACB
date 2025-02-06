7becdeab401e51f5b0d415f227a82253559745c6
KienDao <daokien2303@gmail.com>
2025-02-06 20:08:56 +0700

    update index.js

--- index.js
+++ index.js
@@ -5,32 +5,31 @@ const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/w500';
 const options = {
     method: 'GET',
     headers: {
-      accept: 'application/json',
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjYTc1MTE1YmMyYjMzOWQyOWQyMzZhYWMxNzYzNzZkOCIsIm5iZiI6MTcyOTMxMzIwMi40MDQsInN1YiI6IjY3MTMzOWIyMmJiYmE2NWY3YjEwYThmNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.odq7v3Agi7j8m3bSXoeLCUQ_Rw1KuBec69ZRW6yaFQE'
+        accept: 'application/json',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjYTc1MTE1YmMyYjMzOWQyOWQyMzZhYWMxNzYzNzZkOCIsIm5iZiI6MTcyOTMxMzIwMi40MDQsInN1YiI6IjY3MTMzOWIyMmJiYmE2NWY3YjEwYThmNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.odq7v3Agi7j8m3bSXoeLCUQ_Rw1KuBec69ZRW6yaFQE'
     }
 };
 
-    fetch(API_URL, options)
-        .then(function(response) {
-            return response.json();
-        })
-        .then(function(data) {
-            displayMovies(data.results);
-        })
-        .catch(function(error) {
-            console.error('Lỗi khi tải phim:', error);
-        });
+fetch(API_URL, options)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        displayMovies(data.results);
+    })
+    .catch(function (error) {
+        console.error('Lỗi khi tải phim:', error);
+    });
 
 
-// Function to display movies
 function displayMovies(movies) {
     var moviesContainer = document.getElementById('moviesContainer');
-    moviesContainer.innerHTML = ''; // Clear previous content
+    moviesContainer.innerHTML = '';
 
-    movies.forEach(function(movie) {
+    movies.forEach(function (movie) {
         var movieElement = document.createElement('div');
         movieElement.classList.add('movie-card');
-        movieElement.innerHTML = 
+        movieElement.innerHTML =
             '<img src="' + IMAGE_BASE_URL + movie.poster_path + '" alt="' + movie.title + '">' +
             '<h3>' + movie.title + '</h3>' +
             '<p>⭐ ' + movie.vote_average + ' / 10</p>';
