1e8dd1b76631421f25c5a363bf2c28e6c838c7f4
KienDao <daokien2303@gmail.com>
2025-02-07 14:28:12 +0700

    add more movies

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const API_KEY = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjYTc1MTE1YmMyYjMzOWQyOWQyMzZhYWMxNzYzNzZkOCIsIm5iZiI6MTcyOTMxMzIwMi40MDQsInN1YiI6IjY3MTMzOWIyMmJiYmE2NWY3YjEwYThmNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.odq7v3Agi7j8m3bSXoeLCUQ_Rw1KuBec69ZRW6yaFQE";
-const API_URL = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=vi-VN&page=1`;
+
+const POPULAR_MOVIES_API_URL = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=vi-VN&page=1`;
+const TOP_RATED_MOVIES_API_URL = `https://api.themoviedb.org/3/movie/top_rated?api_key=${API_KEY}&language=vi-VN&page=1`;
+const UPCOMING_MOVIES_API_URL = `https://api.themoviedb.org/3/movie/upcoming?api_key=${API_KEY}&language=vi-VN&page=1`;
+const NOW_PLAYING_MOVIES_API_URL = `https://api.themoviedb.org/3/movie/now_playing?api_key=${API_KEY}&language=vi-VN&page=1`;
 const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/w500';
 
 const options = {
@@ -10,29 +14,37 @@ const options = {
     }
 };
 
-fetch(API_URL, options)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (data) {
-        displayMovies(data.results);
-    })
-    .catch(function (error) {
-        console.error('Lỗi khi tải phim:', error);
-    });
+fetch(NOW_PLAYING_MOVIES_API_URL, options)
+    .then(response => response.json())
+    .then(data => displayMovies(data.results, "nowPLayingMoviesContainer"))
+    .catch(error => console.error("Error fetching movies:", error));
 
+fetch(POPULAR_MOVIES_API_URL, options)
+    .then(response => response.json())
+    .then(data => displayMovies(data.results, "popularMoviesContainer"))
+    .catch(error => console.error("Error fetching movies:", error));
 
-function displayMovies(movies) {
-    var moviesContainer = document.getElementById('moviesContainer');
+fetch(TOP_RATED_MOVIES_API_URL, options)
+    .then(response => response.json())
+    .then(data => displayMovies(data.results, "topRatedMoviesContainer"))
+    .catch(error => console.error("Error fetching movies:", error));
+
+fetch(UPCOMING_MOVIES_API_URL, options)
+    .then(response => response.json())
+    .then(data => displayMovies(data.results, "upComingMoviesContainer"))
+    .catch(error => console.error("Error fetching movies:", error));
+
+function displayMovies(movies, containerId) {
+    var moviesContainer = document.getElementById(containerId);
     moviesContainer.innerHTML = '';
 
     movies.forEach(function (movie) {
         var movieElement = document.createElement('div');
         movieElement.classList.add('movie-card');
-        movieElement.innerHTML =
-            '<img src="' + IMAGE_BASE_URL + movie.poster_path + '" alt="' + movie.title + '">' +
-            '<h3>' + movie.title + '</h3>' +
-            '<p>⭐ ' + movie.vote_average + ' / 10</p>';
+        movieElement.innerHTML = `
+            <img src="${IMAGE_BASE_URL}${movie.poster_path}" alt="${movie.title}">
+            <h3>${movie.title}</h3>
+            <p>⭐ ${movie.vote_average} / 10</p>`;
         moviesContainer.appendChild(movieElement);
     });
 }
